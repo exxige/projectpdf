df581331c40359380a89df4807591f30e6b03064
exxige <153573153+exxige@users.noreply.github.com>
2024-10-22 23:12:08 +0000

    Changed post button to #4dbbff

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -8,7 +8,7 @@ export default function Navigation() {
       <nav>
         <ul>
           <li>
-              <a href="https://heyform.net/f/mYTgNrF4" className="rounded-full border-[1.5px] border-[#a6a6a6] bg-black px-4 py-3 text-sm text-white transition-all hover:bg-black/85">
+              <a href="https://heyform.net/f/mYTgNrF4" className="rounded-full border-[1.5px] border-[#a6a6a6] bg-[#4dbbff] px-4 py-3 text-sm text-white transition-all hover:bg-[#4dbbff]/85">
                 Post SEO resource
               </a>
            
