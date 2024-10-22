85fcf9956126d94a04c50cd9aff901b0a5fb86a5
exxige <153573153+exxige@users.noreply.github.com>
2024-10-22 12:10:41 +0000

    retry env

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Logo from "./Logo";
-import FormModal from "./FormModal";
-import { Button } from "./ui/button";
 
 export default function Navigation() {
   return (
@@ -13,8 +11,7 @@ export default function Navigation() {
               <a href="https://forms.gle/ZgmJxaZD1xzM3B6d9" className="rounded-full border-[1.5px] border-[#a6a6a6] bg-black px-4 py-3 text-sm text-white transition-all hover:bg-black/85">
                 Post SEO resource
               </a>
-            {/* <FormModal>
-            </FormModal> */}
+           
           </li>
         </ul>
       </nav>
